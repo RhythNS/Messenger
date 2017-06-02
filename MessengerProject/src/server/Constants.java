e055ff3af85742b83a83f28aa626845a3ee1ce8a
RhythNS <noahsamuel@web.de>
2017-06-02 17:23:34 +0200

    Bene Hotfix
    - Added additional Values to Constants

--- MessengerProject/src/server/Constants.java
+++ MessengerProject/src/server/Constants.java
@@ -5,13 +5,20 @@ public class Constants {
 	public static final int MAX_GROUP_NAME = 32;
 	public static final int MAX_USERNAME_SIZE = 32;
 	public static final int MAX_PASSWORD_SIZE = 50;
+	public static final int MAX_DEVICES = 50;
+	public static final int MAX_FRIENDS = 256;
+	public static final int MAX_GROUP_MEMBERS = 256;
 
-	public static final int TIMEOUT_IN_SECONDS = 50;
+	public static final int MAX_TIMEOUT_IN_SECONDS = 50;
 
 	public static final int DAYS_GET_FOR_NEW_DEVICE_LOGIN = -2;
 
 	public static final int MESSAGES_DAYS_KEPT_IN_DATAMANAGMENT = 14;
+	public static final int MINIMAL_DAYS_UNTIL_FILE_CLEANING = 1;
+	public static final int MAXIMAL_DAYS_UNTIL_FILE_CLEANING = 3;
 
 	public static final char FILLER = '#';
 	public static final char SEPERATOR = ',';
+	public static final char DELETE_SYMBOL = 'D';
+	public static final int POINTER_SIZE = 6;
 }
