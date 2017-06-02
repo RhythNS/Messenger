13551cae822bfa633684463786daf4777eeee3f9
Alligatooo <rittner.benni@t-online.de>
2017-06-02 18:00:41 +0200

    Classes Account, Server
    Account and Server are not finished yet.

--- MessengerProject/src/server/Account.java
+++ MessengerProject/src/server/Account.java
@@ -1,19 +1,90 @@
 package server;
 
+import com.sun.media.jfxmedia.logging.Logger;
+import dataManagement.DateCalc;
+import dataManagement.FileMessage;
+import dataManagement.Mailbox;
+import dataManagement.TextMessage;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class Account {
 
-	private String username;
-	private int key;
-	private ArrayList<Client> clients;
+	private String password;
 	private int tag;
+	private ArrayList<Client> clients;
+	private Server server;
+	private boolean isLoggedIn = false;
+	private final Object object = new Object();
 
-	public Account(String username, int key, int tag) {
-		this.username = username;
-		this.key = key;
+	public Account(int tag, Server server) {
 		this.tag = tag;
 		clients = new ArrayList<>();
+		this.password = password;
+	}
+
+	public void recieveMessage(Account from, String message, Client whoGotIt){
+		synchronized (object) {
+			String date = DateCalc.getMessageDate();
+			for (Client c : clients) {
+				if (!whoGotIt.equals(c))
+					c.write(tag, from.tag, date, message);
+			}
+
+			server.recieveMessage(from,this,message,date);
+		}
+	}
+
+	public void requestMessage(Client sender,String date){
+		Mailbox mb = server.requestMessage(this, date);
+		if(mb == null)return;
+		for (int i = 0; i < mb.messageSize(); i++) {
+			TextMessage tm = mb.getMessage(i);
+			sender.write(tm.getTo(),tm.getFrom(),tm.getDate(),tm.getContent());
+		}
+		for (int i = 0; i < mb.fileSize(); i++) {
+			FileMessage fm = mb.getFile(i);
+			try {
+				sender.write(fm.getFrom(),fm.getTo(),fm.getDate(),new FileInputStream(fm.getContent()));
+			} catch (FileNotFoundException e) {
+				dataManagement.Logger.getInstance().log("Acc002: Cannot find the File");
+			}
+		}
+
+	}
+
+	public void acceptFriend(int tag){
+		synchronized (object){
+			server.addFriendTo(this,tag);
+
+		}
+	}
+
+	public void sendMessage(String message, Account toAccount, Client sender){
+		for (Client client: clients) {
+			if(!client.equals(sender)){
+				client.write(toAccount.getTag(), tag, DateCalc.getMessageDate(),message);
+			}
+		}
+
+	}
+
+	public void addToFriendlist(int tagToAdd){
+		server.addFriendTo(this, tagToAdd );
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int createGroup(String nameOfGroup, Account[] accounts) {
+		if (nameOfGroup == null || accounts == null) {
+			dataManagement.Logger.getInstance().log("Acc001: ToCreate Group there is something null in createGroup()");
+			return 0;
+		}
+		return server.createGroup(nameOfGroup, accounts);
 	}
 
 	public int getTag() {
--- MessengerProject/src/server/Server.java
+++ MessengerProject/src/server/Server.java
@@ -1,64 +1,156 @@
 package server;
 
 import java.io.IOException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.util.ArrayList;
 
 import dataManagement.DataManagement;
-import secruity.KeyConverter;
+import dataManagement.DateCalc;
+import dataManagement.Logger;
+import dataManagement.Mailbox;
+import secruity.KeyStoreSynchron;
 import socketio.ServerSocket;
 import socketio.Socket;
 
+import javax.crypto.SecretKey;
+
 public class Server {
 
-	private Account admin;
+	private final char[] passwordForData;
 	private ArrayList<Account> accounts;
-	private ArrayList<Group> groups;
 	private Thread acceptingThread;
 	private ServerSocket server;
 	private DataManagement dataMangement;
 	private boolean isRunning = false;
+	private final Object userlock = new Object();
+	private final SecretKey secretKey;
 
-	public Server() {
+	public Server(char[] password) {
+		dataMangement = new DataManagement(null);
+		this.passwordForData = password;
+		if(KeyStoreSynchron.getInstance().loadKeyStore(password)){
+			this.secretKey = KeyStoreSynchron.getInstance().getKey(password);
+			startServer();
+		}
+		else{
+			secretKey= null;
+		}
+	}
 
+	private void startServer() {
+		accounts = new ArrayList<>();
+		isRunning = true;
+		startAcceptingThread();
 	}
 
 	private void startAcceptingThread() {
 		if (!isRunning) {
 			isRunning = true;
-			acceptingThread = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					while (isRunning) {
-						Socket socket = null;
-						try {
-							socket = server.accept();
-						} catch (IOException e) {
-							System.err.println("Something went wrong with accepting the socket! #BlameBene");
-							e.printStackTrace();
-							continue;
-						}
-						// TODO GENERATE PRIVATE AND PUBLIC KEYS
-						try {
-							PublicKey userPublicKey = KeyConverter.generatePublicKey(socket.readLine());
-						} catch (IOException e) {
-							System.err.println("Something went wrong with getting the PublicKey! #BlameBene");
-							e.printStackTrace();
-							continue;
-						}
-					}
-				}
-			});
+			Server serverInstance = this;
+			acceptingThread = new Thread(() -> {
+                while (isRunning) {
+                    Socket socket;
+                    try {
+                        socket = server.accept();
+                        new Client(socket, serverInstance);
+                    } catch (IOException e) {
+                        System.err.println("Something went wrong with accepting the socket! #BlameBene");
+                        e.printStackTrace();
+
+                        continue;
+                    }
+                }
+            });
 		}
+	}
+
+	/**
+	 * Tries to register a new User
+	 *
+	 * @return returns an Account of the registered User
+	 * 			returns null, if something went wrong
+	 */
+
+	public Account registerUser(String userPass) {
+		synchronized (userlock) {
+			if (userPass == null) {
+				Logger.getInstance().log("Ser001: User cannot register because UserPass is null!");
+				return null;
+			}
+			String[] informations = userPass.split(";");
+			int tag = dataMangement.registerUser(informations[0], informations[1]);
+			if(tag == 0){
+				return null;
+			}
+			Account account = new Account(tag,this);
+			accounts.add(account);
+
+			return account;
+
+		}
+	}
+
+	/**
+	 *
+	 * @param username required for login
+	 * @param passwort required for login
+	 * @return if login can be done returns the Account
+	 * 			if the Account is already online it just adds a CLient to this Account --> returns Account
+	 *
+	 * 		if the login went wrong somehow --> returns null
+	 */
+	public Account loginAccount(String username,String passwort) {
+
+		synchronized (userlock) {
+			int tag = dataMangement.login(username, passwort);
+			if (tag == -1) {
+				Logger.getInstance().log("Ser003: Cannot find the fitting tag to log in @loginAccount()!");
+				return null;
+			}
+
+			for (Account account: accounts) {
+				if(account.getTag() == tag){
+					return account;
+				}
+			}
+			Account a = new Account(tag,this);
+			accounts.add(a);
+			return a;
+		}
+	}
+
+	public void sendMessage(String message, Account toAccount, Account fromAccount){
+
+
+		dataMangement.saveMessage(fromAccount.getTag(),toAccount.getTag(), DateCalc.getDeviceDate(),message);
+
 	}
 
 	protected void connectUserAccount(Account account) {
 
 	}
 
-	protected void createGroup(Account[] accounts) {
+	protected int createGroup(String name, Account[] accounts) {
+		int[] tags = new int[accounts.length];
+
+		for (int i = 0; i < tags.length; i++) {
+			tags[i] = accounts[i].getTag();
+		}
+		return dataMangement.createGroup(name,tags);
+	}
+
+
+	//TODO save new Friend to Datamanagement
+	public boolean addFriendTo(Account account,int tagOfAccountToAdd) {
+		return dataMangement.addFriend(account.getTag(),tagOfAccountToAdd);
+	}
+
+	public void recieveMessage(Account from, Account to, String message, String date) {
+
+		dataMangement.saveMessage(from.getTag(),to.getTag(),date,message);
 
 	}
 
+	public Mailbox requestMessage(Account sender, String date) {
+		return dataMangement.getMessages(sender.getTag(),date);
+	}
 }
